ae1cc08a1fdb452a4d4a16e8d1c63b6b19198d50
ujjal38 <kundu.vivasoft@bkash.com>
2023-09-22 21:35:38 +0600

    Init profile maker project

---
+++ src/main/java/com/sonic/profilemaker/ProfileMakerApplication.java
@@ -0,0 +1,13 @@
+package com.sonic.profilemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonic/profilemaker/ProfileMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonic.profilemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
